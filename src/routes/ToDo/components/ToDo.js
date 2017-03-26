d9440bd88def67223b170abfa3c4df633eb3ccd1
MichalObi <ndwz123@gmail.com>
2017-03-26 21:20:38 +0200

    template for todo app

--- src/routes/ToDo/components/ToDo.js
+++ src/routes/ToDo/components/ToDo.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import {addTodo} from '../modules/ToDo.js'
 
 export const ToDo = () => (
   <div>
     <h2>ToDo</h2>
+    <div id="to-do">
+      <div id="add-to-do">
+        <input type="text" placeholder="add to do"></input>
+        <button onClick={addTodo}>Add to do</button>
+      </div>
+      <div id="to-do-list">
+        <ul>
+         <li>to do one</li>
+         <li>to do two</li>
+         <li>to to three</li>
+        </ul>
+      </div>
+      <div id="to-do-filter">
+        <ul>
+          <li>Show:</li>
+          <li>All</li>
+          <li>Active</li>
+          <li>Complited</li>
+        </ul>
+      </div>
+    </div>
   </div>
 )
 
--- src/routes/ToDo/modules/ToDo.js
+++ src/routes/ToDo/modules/ToDo.js
@@ -1,4 +1,9 @@
 const initialState = 0
+
+export const addTodo = () => {
+  console.log('text');
+}
+
 export default function toDoReducer (state = initialState, action) {
   return true
 }
